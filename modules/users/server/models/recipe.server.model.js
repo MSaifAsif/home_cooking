34160db6062029a5c6e56784ab1dc273d33d799c
mean <saifasifmirza@gmail.com>
2016-05-08 20:59:28 +0500

    Project strucutre

---
+++ modules/users/server/models/recipe.server.model.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+/**
+ * An initial level validation check
+ * on title to be of 15 chars only
+ */
+function validateTitle(title) {
+  return title.length <= 150;
+}
+
+function validateSteps(stepsArray) {
+  for (var i = stepsArray.length - 1; i >= 0; i--) {
+    if (typeof(stepsArray[i]) === 'string' || stepsArray[i] instanceof String) {
+      // do nothing
+    } else {
+      return false;
+    }
+  }
+}
+
+/**
+ * Recipe Schema
+ */
+var RecipeSchema = new Schema({
+  // Recipe model fields   
+  // ...
+  id: {
+    type: Schema.Types.ObjectId,
+    default: new mongoose.Types.ObjectId()
+  },
+  title: {
+    type: String,
+    validate: [validateTitle, 'Title should not exceed 15 characters']
+  },
+  steps: {
+    type: [String]
+  },
+  lastUpdated: {
+    type: Date, 
+    default: Date.now()
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  media: {
+    type: Schema.Types.Mixed,
+    default: {}
+  }
+});
+
+mongoose.model('Recipe', RecipeSchema);
---
+++ modules/users/server/routes/recipe.server.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(app) {
+  var recipe = require('../controllers/recipe.server.controller');
+
+  app.route('/recipes/all').get(recipe.list);
+  app.route('/recipes/add').post(recipe.create);
+
+  // filter queries
+  app.route('/recipes/find_any').get(recipe.find_any);
+  app.route('/recipes/find_only').get(recipe.find_only);
+
+};
