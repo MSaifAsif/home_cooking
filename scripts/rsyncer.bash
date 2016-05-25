3445e9822ea914afa8680f04a70da2e8b763faac
mean <saifasifmirza@gmail.com>
2016-05-25 09:32:25 +0500

    recipe details pags

--- scripts/rsyncer.bash
+++ scripts/rsyncer.bash
@@ -2,5 +2,5 @@
 while true
 do
  sleep 5
- rsync /media/sf_home_cooking_parent/* /home/mean/projects/home_cooking/ -rvua
+ rsync /media/sf_home_cooking_parent/* /home/mean/projects/home_cooking/ -rva
 done
