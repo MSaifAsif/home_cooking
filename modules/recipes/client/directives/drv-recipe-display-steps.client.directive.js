8baf8cef5dd293604c22b115e593bece3a63b63a
mean <saifasifmirza@gmail.com>
2016-05-27 15:55:38 +0500

    cleanup and template fixing

--- modules/recipes/client/directives/drv-recipe-display-steps.client.directive.js
+++ modules/recipes/client/directives/drv-recipe-display-steps.client.directive.js
@@ -8,6 +8,24 @@
     drvRecipeDisplaySteps.$inject = ['$parse', '$compile'];
 
     function drvRecipeDisplaySteps($parse, $compile) {
+        var evenTemplate =   '<div class="media-left">' +
+                                '<a href="#">' +
+                                  '<img class="media-object" src="http://www.healthyseasonalrecipes.com/wp-content/uploads/2013/03/roasted-garlic-hummus-119.jpg" alt="aaa" height="200">' +
+                                '</a>' +
+                              '</div>' +
+                              '<div class="media-body media-middle">' +
+                                '<span class="para" >{{recipeDirection}}</span>' +
+                              '</div>';
+
+        var oddTemplate = '<div class="media-body media-middle">' +
+                                '<span class="para" >{{recipeDirection}}</span>' +
+                              '</div>' +
+                              '<div class="media-right">' +
+                                '<a href="#">' +
+                                  '<img class="media-object" src="http://www.healthyseasonalrecipes.com/wp-content/uploads/2013/03/roasted-garlic-hummus-119.jpg" alt="aaa" height="200">' +
+                                '</a>' +
+                              '</div>';
+
         return {
             restrict: 'AE',
             replace: 'false',
@@ -15,38 +33,8 @@
                 recipeDirection: '@',
                 recipeNumber: '@'
             },
-            // template: '<h3>{{recipeDirection}}</h3>',
-            // templateUrl: function (tElement, tAttrs) {
-            //     if (tAttrs) {
-            //         console.log(tAttrs.recipeNumber);
-            //         if (tAttrs.recipeNumber % 2 === 0) {
-            //             return '../views/pub/fragments/recipe-even-step-template.html';
-            //         } else {
-            //             return '../views/pub/fragments/recipe-odd-step-template.html';
-            //         }
-            //     }
-            // },
             link: function(scope, element, attrs) {
-                // console.log(scope.recipeDirection);
-
                 var template;
-                var evenTemplate =   '<div class="media-left">' +
-                                        '<a href="#">' +
-                                          '<img class="media-object" src="http://www.healthyseasonalrecipes.com/wp-content/uploads/2013/03/roasted-garlic-hummus-119.jpg" alt="aaa" height="200">' +
-                                        '</a>' +
-                                      '</div>' +
-                                      '<div class="media-body media-middle">' +
-                                        '<span class="para" >{{recipeDirection}}</span>' +
-                                      '</div>';
-
-                var oddTemplate = '<div class="media-body media-middle">' +
-                                        '<span class="para" >{{recipeDirection}}</span>' +
-                                      '</div>' +
-                                      '<div class="media-right">' +
-                                        '<a href="#">' +
-                                          '<img class="media-object" src="http://www.healthyseasonalrecipes.com/wp-content/uploads/2013/03/roasted-garlic-hummus-119.jpg" alt="aaa" height="200">' +
-                                        '</a>' +
-                                      '</div>';
                 if (scope.recipeNumber % 2 === 0) {
                     console.log('even');
                     template = $compile(evenTemplate)(scope);
