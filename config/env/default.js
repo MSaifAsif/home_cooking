1233eef0b2d358c4a17a1ba8d71cbe3a55105f2f
MSaifAsif <saifasifmirza@gmail.com>
2016-08-06 13:54:15 +0500

    styling fixes

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     app: {
-        title: 'MEAN.JS',
-        description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-        keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+        title: 'Home Cooking',
+        description: 'Mama\'s very own kitchen',
+        keywords: 'home, cooking, recipes, desi, kitchen',
         googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
     },
     port: process.env.PORT || 3000,
